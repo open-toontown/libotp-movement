4d784167d4365ccfdd32114a891f7431ea6913ac
John Cote <jwcotejr@gmail.com>
2019-03-29 13:23:13 -0400

    Update CMover.py

--- libotp/movement/CMover.py
+++ libotp/movement/CMover.py
@@ -12,8 +12,10 @@ class CMover:
         self.VecType = Vec3(0, 0, 0)
         self.dt = 1.0
         self.dtClock = globalClock.getFrameTime()
-        self.movement = Vec3(0, 0, 0)
-        self.rotation = Vec3(0, 0, 0)
+        self.shove = Vec3(0, 0, 0)
+        self.rotShove = Vec3(0, 0, 0)
+        self.force = Vec3(0, 0, 0)
+        self.rotForce = Vec3(0, 0, 0)
         self.cImpulses = {}
 
     def setFwdSpeed(self, fwdSpeed):
@@ -28,11 +30,16 @@ class CMover:
     def getRotSpeed(self):
         return self.rotSpeed
 
+    def setNodePath(self, objNodePath):
+        self.objNodePath = objNodePath
+
     def getNodePath(self):
         return self.objNodePath
 
-    def processCImpulses(self, dt):
-        self.dt = dt
+    def processCImpulses(self, dt=None):
+        if dt is not None:
+            self.dt = dt
+
         if self.getDt() == -1.0:
             clockDelta = globalClock.getFrameTime()
             self.dt = clockDelta - self.dtClock
@@ -41,21 +48,35 @@ class CMover:
         for cImpulse in self.cImpulses.values():
             cImpulse.process(self.getDt())
 
+    def setDt(self, dt):
+        self.dt = dt
+        if self.getDt() == -1.0:
+            clockDelta = globalClock.getFrameTime()
+            selt.dt = clockDelta - self.dtClock
+            self.dtClock = clockDelta
+
     def getDt(self):
         return self.dt
+
+    def resetDt(self):
+        clockDelta = globalClock.getFrameTime()
+        self.dtClock = clockDelta
 
     def integrate(self):
         if not self.objNodePath or self.objNodePath.isEmpty():
             return
 
-        self.movement *= self.getDt()
-        self.objNodePath.setFluidPos(self.objNodePath, self.movement)
-        self.rotation *= self.getDt()
-        self.objNodePath.setHpr(self.objNodePath, self.rotation)
-        self.movement = Vec3(0, 0, 0)
-        self.rotation = Vec3(0, 0, 0)
+        self.shove *= self.getDt()
+        self.objNodePath.setFluidPos(self.objNodePath, self.shove)
+        self.rotShove *= self.getDt()
+        self.objNodePath.setHpr(self.objNodePath, self.rotShove)
+        self.shove = Vec3(0, 0, 0)
+        self.rotShove = Vec3(0, 0, 0)
 
     def addCImpulse(self, name, cImpulse):
+        if not cImpulse:
+            return
+
         self.removeCImpulse(name)
         self.cImpulses[name] = cImpulse
         cImpulse.setMover(self)
@@ -70,7 +91,16 @@ class CMover:
         return False
 
     def addShove(self, shove):
-        self.movement += shove
+        self.shove += shove
 
     def addRotShove(self, rotShove):
-        self.rotation += rotShove
+        self.rotShove += rotShove
+
+    def addForce(self, force):
+        self.force += force
+
+    def addRotForce(self, rotForce):
+        self.rotForce += rotForce
+
+    def getCImpulse(self, name):
+        return self.cImpulses.get(name)
