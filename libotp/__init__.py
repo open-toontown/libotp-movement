ff21d48bce652c5be600dc99e5fcf9188ab7b838
John Cote <jwcotejr@users.noreply.github.com>
2019-05-15 18:53:28 -0400

    Update __init__.py

--- libotp/__init__.py
+++ libotp/__init__.py
@@ -1,3 +1,3 @@
-from movement.CImpulse import CImpulse
-from movement.CMover import CMover
-from movement.CMoverGroup import CMoverGroup
+from .movement.CImpulse import CImpulse
+from .movement.CMover import CMover
+from .movement.CMoverGroup import CMoverGroup
